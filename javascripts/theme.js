cf9bd971537ca50b7c263e7fd6dc66b4de68b81a
Ludovic Meyer <ludovic.meyer@pixel-cookers.com>
2013-04-14 19:33:56 +0200

    Add stopwatch javascript tool

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,3 +1,132 @@
+/**
+ * Pixel Cookers script for Redmine
+ * author: Ludovic Meyer, Pixel Cookers - http://www.pixel-cookers.com
+ * v1.4
+ */
+(function($, document, undefined) {
+	$(document).ready(function() {
+		var cfg = {
+			"password.length": 7,
+			"password.special": false,
+			"stopwatch.enabled": true,
+			"stopwatch.precision": 2
+		};
+
+		var sidebar_btn = $('<div id="sidebar_btn">&nbsp;</div>'),
+			elem = $('#main:not(.nosidebar) #sidebar');
+
+		sidebar_btn.on('click', toggle_sidebar);
+
+		if (elem != undefined){
+			elem.before(sidebar_btn);
+			if ($.cookie('hide_sidebar') == 'yes'){
+				$('#main').toggleClass('nosidebar');
+			}
+		}
+
+		injectViewportMetaTag();
+		injectAppleTouchIcons();
+
+		// Password generation in user creation
+		var genContainer = $(document.createElement('p'));
+		var genLabel = $(document.createElement('label'));
+		var genBtn = $(document.createElement('input')).attr('id', 'generate-password').attr('type', "submit").attr('value', 'Generate password');
+		var useBtn = $(document.createElement('input')).attr('id', 'use-password').attr('type', "submit").attr('value', 'Use password');
+		var passFields = $('.controller-users.action-new #password_fields, .controller-users.action-create #password_fields, .controller-users.action-edit #password_fields');
+
+		passFields.append(genContainer);
+		genContainer.append(genLabel).append(genBtn).append(useBtn);
+		useBtn.hide();
+
+
+		genBtn.on('click', function(){
+			var password = $.password(cfg['password.length'],cfg['password.special']);
+			var info = passFields.find('.info');
+			useBtn.fadeIn().attr('attr-value', password);
+			info.html('Password generated : '+password);
+			$('#user_password, #user_password_confirmation').val('');
+			return false;
+		});
+
+		useBtn.on('click', function(){
+			$('#user_password, #user_password_confirmation').val($(this).attr('attr-value'));
+			$(this).fadeOut();
+			return false;
+		});
+
+		if(cfg['stopwatch.enabled']) {
+			var stopwatchContainer = $(document.createElement('div')).attr('class', 'stopwatch-container');
+			var runBtn = $(document.createElement('input')).attr('id', 'stopwatch-run').attr('class', 'greydient').attr('type', "submit").attr('value', 'Play');
+			var stopwatch = $(document.createElement('span')).attr('id', 'stopwatch').attr('class', 'icon icon-time');
+			$('#time_entry_hours').parent().append(stopwatchContainer);
+			stopwatchContainer.append(stopwatch).append(runBtn);
+			stopwatch.html('00:00:00');
+
+
+			$('#stopwatch-run').on('click', function(){
+				stopwatch.stopwatch().stopwatch('toggle');
+				stopwatch.bind('tick.stopwatch', function(e, elapsed){
+					var num = elapsed/3600000;
+					$('#time_entry_hours').attr('value', num.toFixed(cfg['stopwatch.precision']));
+				})
+
+				if($(this).attr('value') == 'Play') {
+					$(this).attr('value', 'Pause');
+				} else {
+					$(this).attr('value', 'Play');
+				}
+
+				return false;
+			});
+		}
+	});
+
+	function get_theme_config() {
+
+	}
+
+	function toggle_sidebar(){
+		$('#main').toggleClass('nosidebar');
+		if($('#main').hasClass('nosidebar')){
+			$.cookie('hide_sidebar', 'yes');
+		}else{
+			$.cookie('hide_sidebar', 'no');
+		}
+	}
+
+	function injectViewportMetaTag(){
+		var meta = $(document.createElement('meta'));
+		meta.attr('name', 'viewport');
+		meta.attr('content', 'width=device-width, initial-scale=1');
+		$('head').append(meta);
+	}
+
+	var scripts = document.getElementsByTagName("script"),
+		src = scripts[scripts.length-1].src;
+	src = src.split('/');
+	src = src[src.length-3];
+
+	function injectAppleTouchIcons(){
+		var link = $(document.createElement('link'));
+		link.attr('rel', 'apple-touch-icon');
+		link.attr('href', '/themes/'+src+'/images/touch/apple-touch-icon.png');
+		$('head').append(link);
+
+		link = $(document.createElement('link'));
+		link.attr('rel', 'apple-touch-icon');
+		link.attr('href', '/themes/'+src+'/images/touch/apple-touch-icon-72x72-precomposed.png');
+		link.attr('sizes', '72x72');
+		$('head').append(link);
+
+		link = $(document.createElement('link'));
+		link.attr('rel', 'apple-touch-icon');
+		link.attr('href', '/themes/'+src+'/images/touch/apple-touch-icon-114x114-precomposed.png');
+		link.attr('sizes', '114x114');
+		$('head').append(link);
+	}
+
+})(jQuery, document);
+
 /*!
  * jQuery Cookie Plugin v1.3
  * https://github.com/carhartl/jquery-cookie
@@ -99,104 +228,160 @@ $.extend({
 	}
 });
 
-/**
- * Pixel Cookers script for Redmine
- * author: Ludovic Meyer, Pixel Cookers - http://www.pixel-cookers.com
- * v1.3
+/*
+ * jQuery stopwatch
+ * https://github.com/robcowie/jquery-stopwatch/blob/master/jquery.stopwatch.js
  */
-(function($, document, undefined) {
-	$(document).ready(function() {
-		var cfg = {
-			"password.length": 7,
-			"password.special": false
+(function( $ ){
+
+	function incrementer(ct, increment) {
+		return function() { ct+=increment; return ct; };
+	}
+
+	function pad2(number) {
+		return (number < 10 ? '0' : '') + number;
+	}
+
+	function defaultFormatMilliseconds(millis) {
+		var x, seconds, minutes, hours;
+		x = millis / 1000;
+		seconds = Math.floor(x % 60);
+		x /= 60;
+		minutes = Math.floor(x % 60);
+		x /= 60;
+		hours = Math.floor(x % 24);
+		// x /= 24;
+		// days = Math.floor(x);
+		return [pad2(hours), pad2(minutes), pad2(seconds)].join(':');
+	}
+
+	//NOTE: This is a the 'lazy func def' pattern described at http://michaux.ca/articles/lazy-function-definition-pattern
+	function formatMilliseconds(millis, data) {
+		// Use jintervals if available, else default formatter
+		var formatter;
+		if (typeof jintervals == 'function') {
+			formatter = function(millis, data){return jintervals(millis/1000, data.format);};
+		} else {
+			formatter = defaultFormatMilliseconds;
+		}
+		formatMilliseconds = function(millis, data) {
+			return formatter(millis, data);
 		};
+		return formatMilliseconds(millis, data);
+	}
 
+	var methods = {
 
-		var sidebar_btn = $('<div id="sidebar_btn">&nbsp;</div>'),
-			elem = $('#main:not(.nosidebar) #sidebar');
+		init: function(options) {
+			var defaults = {
+				updateInterval: 1000,
+				startTime: 0,
+				format: '{HH}:{MM}:{SS}',
+				formatter: formatMilliseconds
+			};
 
-		sidebar_btn.on('click', toggle_sidebar);
+			// if (options) { $.extend(settings, options); }
 
-		if (elem != undefined){
-			elem.before(sidebar_btn);
-			if ($.cookie('hide_sidebar') == 'yes'){
-				$('#main').toggleClass('nosidebar');
-			}
+			return this.each(function() {
+				var $this = $(this),
+					data = $this.data('stopwatch');
+
+				// If the plugin hasn't been initialized yet
+				if (!data) {
+					// Setup the stopwatch data
+					var settings = $.extend({}, defaults, options);
+					data = settings;
+					data.active = false;
+					data.target = $this;
+					data.elapsed = settings.startTime;
+					// create counter
+					data.incrementer = incrementer(data.startTime, data.updateInterval);
+					data.tick_function = function() {
+						var millis = data.incrementer();
+						data.elapsed = millis;
+						data.target.trigger('tick.stopwatch', [millis]);
+						data.target.stopwatch('render');
+					};
+					$this.data('stopwatch', data);
+				}
+
+			});
+		},
+
+		start: function() {
+			return this.each(function() {
+				var $this = $(this),
+					data = $this.data('stopwatch');
+				// Mark as active
+				data.active = true;
+				data.timerID = setInterval(data.tick_function, data.updateInterval);
+				$this.data('stopwatch', data);
+			});
+		},
+
+		stop: function() {
+			return this.each(function() {
+				var $this = $(this),
+					data = $this.data('stopwatch');
+				clearInterval(data.timerID);
+				data.active = false;
+				$this.data('stopwatch', data);
+			});
+		},
+
+		destroy: function() {
+			return this.each(function(){
+				var $this = $(this),
+					data = $this.data('stopwatch');
+				$this.stopwatch('stop').unbind('.stopwatch').removeData('stopwatch');
+			});
+		},
+
+		render: function() {
+			var $this = $(this),
+				data = $this.data('stopwatch');
+			$this.html(data.formatter(data.elapsed, data));
+		},
+
+		getTime: function() {
+			var $this = $(this),
+				data = $this.data('stopwatch');
+			return data.elapsed;
+		},
+
+		toggle: function() {
+			return this.each(function() {
+				var $this = $(this);
+				var data = $this.data('stopwatch');
+				if (data.active) {
+					$this.stopwatch('stop');
+				} else {
+					$this.stopwatch('start');
+				}
+			});
+		},
+
+		reset: function() {
+			return this.each(function() {
+				var $this = $(this);
+				data = $this.data('stopwatch');
+				data.incrementer = incrementer(data.startTime, data.updateInterval);
+				data.elapsed = data.startTime;
+				$this.data('stopwatch', data);
+			});
 		}
-
-		injectViewportMetaTag();
-		injectAppleTouchIcons();
-
-		// Password generation in user creation
-		var genContainer = $(document.createElement('p'));
-		var genLabel = $(document.createElement('label'));
-		var genBtn = $(document.createElement('input')).attr('id', 'generate-password').attr('type', "submit").attr('value', 'Generate password');
-		var useBtn = $(document.createElement('input')).attr('id', 'use-password').attr('type', "submit").attr('value', 'Use password');
-		var passFields = $('.controller-users.action-new #password_fields, .controller-users.action-create #password_fields, .controller-users.action-edit #password_fields');
-
-		passFields.append(genContainer);
-		genContainer.append(genLabel).append(genBtn).append(useBtn);
-		useBtn.hide();
+	};
 
 
-		genBtn.on('click', function(){
-			var password = $.password(cfg['password.length'],cfg['password.special']);
-			var info = passFields.find('.info');
-			useBtn.fadeIn().attr('attr-value', password);
-			info.html('Password generated : '+password);
-			$('#user_password, #user_password_confirmation').val('');
-			return false;
-		});
-
-		useBtn.on('click', function(){
-			$('#user_password, #user_password_confirmation').val($(this).attr('attr-value'));
-			$(this).fadeOut();
-			return false;
-		});
-	});
-
-	function get_theme_config() {
-
-	}
-
-	function toggle_sidebar(){
-		$('#main').toggleClass('nosidebar');
-		if($('#main').hasClass('nosidebar')){
-			$.cookie('hide_sidebar', 'yes');
-		}else{
-			$.cookie('hide_sidebar', 'no');
+	// Define the function
+	$.fn.stopwatch = function( method ) {
+		if (methods[method]) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.stopwatch' );
 		}
-	}
+	};
 
-	function injectViewportMetaTag(){
-		var meta = $(document.createElement('meta'));
-		meta.attr('name', 'viewport');
-		meta.attr('content', 'width=device-width, initial-scale=1');
-		$('head').append(meta);
-	}
-
-	var scripts = document.getElementsByTagName("script"),
-		src = scripts[scripts.length-1].src;
-	src = src.split('/');
-	src = src[src.length-3];
-
-	function injectAppleTouchIcons(){
-		var link = $(document.createElement('link'));
-		link.attr('rel', 'apple-touch-icon');
-		link.attr('href', '/themes/'+src+'/images/touch/apple-touch-icon.png');
-		$('head').append(link);
-
-		link = $(document.createElement('link'));
-		link.attr('rel', 'apple-touch-icon');
-		link.attr('href', '/themes/'+src+'/images/touch/apple-touch-icon-72x72-precomposed.png');
-		link.attr('sizes', '72x72');
-		$('head').append(link);
-
-		link = $(document.createElement('link'));
-		link.attr('rel', 'apple-touch-icon');
-		link.attr('href', '/themes/'+src+'/images/touch/apple-touch-icon-114x114-precomposed.png');
-		link.attr('sizes', '114x114');
-		$('head').append(link);
-	}
-
-})(jQuery, document);
+})( jQuery );
